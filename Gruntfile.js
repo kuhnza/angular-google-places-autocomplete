fbdd36c509fcb837563cae2444ec38de0f34137a
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2015-09-03 13:01:44 +1000

    Removed unneeded comma

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
 
     karma: {
       options: {
-        configFile: 'karma.conf.js',
+        configFile: 'karma.conf.js'
       },
       unit: {
         browsers: ['PhantomJS'],
